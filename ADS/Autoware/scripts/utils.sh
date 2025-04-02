68e6095e24bc66807b80cef35e9c8461e74637fd
jlds15 <jlchen2020@yahoo.co.jp>
2025-04-02 21:30:12 +0900

    add: util funtion `check_commands_exist`

--- ADS/Autoware/scripts/utils.sh
+++ ADS/Autoware/scripts/utils.sh
@@ -16,3 +16,17 @@ fatal() {
     printf '%s\n' " x $*, aborting ..." >&2
     exit 1
 }
+
+check_commands_exist() {
+    missing=""
+    for cmd in $1; do
+        if [ ! -x "$(command -v "$cmd")" ]; then
+            missing="$missing $cmd"
+        fi
+    done
+    if [ -n "$missing" ]; then
+        error "commands not found:$missing"
+        return 1
+    fi
+    return 0
+}
